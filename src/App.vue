9c15cf989592a47f2118b95048a0264df9f5ba2f
Garifullin Anton <g42@MacBook-Air-G42.local>
2026-05-20 11:43:26 +0700

    feat: gameWord component

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,45 @@
   <div class="game-container">
     <GameFigure />
     <GameWrongLetters />
-    <GameWord/>
+    <GameWord
+      :word="word"
+      :correctLetters="correctLetters"
+    />
   </div>
 
-  <GamePopup />
+  <GamePopup v-if="false" />
 
   <GameNotification />
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 import GameHeader from '@/components/GameHeader.vue'
 import GameFigure from '@/components/GameFigure.vue'
 import GameWrongLetters from '@/components/GameWrongLetters.vue'
 import GameWord from '@/components/GameWord.vue'
 import GamePopup from '@/components/GamePopup.vue'
 import GameNotification from '@/components/GameNotification.vue'
+
+const word = ref<string>('василий');
+const letters = ref<string[]>([]);
+
+const correctLetters = computed((): string[] => letters.value.filter(l => word.value.includes(l)))
+
+const handleKeydown = (event: KeyboardEvent) => {
+  const { key } = event;
+  if (/[а-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase());
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeydown)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeydown)
+})
 </script>
 
 <style scoped></style>
--- src/components/GameWord.vue
+++ src/components/GameWord.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="word">
-    <span class="letter">л</span>
-    <span class="letter"></span>
-    <span class="letter">д</span>
-    <span class="letter"></span>
-    <span class="letter"></span>
+    <span
+      class="letter"
+      v-for="(letter, index) in word"
+      :key="index"
+    >
+      {{ correctLetters.includes(letter) ? letter : '' }}
+    </span>
   </div>
 </template>
 <script setup lang="ts">
+  interface Props {
+    word: string
+    correctLetters: string[],
+  }
+
+  defineProps<Props>()
 </script>
